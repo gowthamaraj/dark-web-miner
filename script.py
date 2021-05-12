f743b5ec728e8ee7f9349a3a64ee780dcd8a5256
gowthamaraj <gowthamaraj360@gmail.com>
2021-05-12 16:15:45 -0400

    add v1

---
+++ script.py
@@ -0,0 +1,80 @@
+import socks
+import socket
+import requests
+import re
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from bs4 import BeautifulSoup
+from bs4.element import Comment
+from urllib.request import urlopen
+
+socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
+socket.socket = socks.socksocket
+
+import yake
+kw_extractor = yake.KeywordExtractor()
+language = "en"
+max_ngram_size = 2
+deduplication_thresold = 0.9
+deduplication_algo = 'seqm'
+windowSize = 1
+numOfKeywords = 20
+
+def getaddrinfo(*args):
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+socket.getaddrinfo = getaddrinfo
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]','script','a']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
+
+html = urlopen('http://jncyepk6zbnosf4p.onion/onions.html').read()
+soup = BeautifulSoup(html, "html.parser")
+pre_tags = soup.findAll('pre',text=True)
+
+onions = []
+for i, pre_tag in enumerate(pre_tags): 
+	try:
+		if i!=0:
+			text = (pre_tag.text)
+			onion_unfiltered = (text.split("\u2003\u2003\u2003\u2003"))
+			if onion_unfiltered[1].endswith('onion') and onion_unfiltered[3] == '200':
+				onions.append(onion_unfiltered[1])
+	except Exception as E:
+		pass
+
+output = {}
+custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_thresold, dedupFunc=deduplication_algo, windowsSize=windowSize, top=numOfKeywords, features=None)
+
+def tag(onion):
+	try:
+		html = urlopen('http://'+onion).read()
+		text = text_from_html(html)
+		keywords = custom_kw_extractor.extract_keywords(text)
+		for kw in keywords:
+			if onion not in output:
+				output[onion] = [kw[0]]
+			else: 
+				output[onion].append(kw[0])
+	except Exception as E:
+		pass
+
+processes = []
+with ThreadPoolExecutor(max_workers=5) as executor:
+	for onion in onions[:5]:
+        	processes.append(executor.submit(tag, onion))
+
+for task in as_completed(processes):
+	task.result()
+
+print(output)
